710b981a860c45f6102bb475b1e479e7f615fd74
FredL <Flaforest@laforest.biz>
2022-10-28 14:36:59 -0400

    Correct project for VS2022

--- DBBackfillDll/Properties/AssemblyInfo.cs
+++ DBBackfillDll/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("3.0.2210.28003")]
-[assembly: AssemblyFileVersion("3.0.2210.28003")]
+[assembly: AssemblyVersion("3.0.2210.28004")]
+[assembly: AssemblyFileVersion("3.0.2210.28004")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
